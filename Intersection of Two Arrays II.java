f48f31e86f578f150cf4da5996bfebfe9b3a4df5
Annie Valentina A <Annievalentina1307@gmail.com>
2026-01-08 21:27:15 +0530

    Create Intersection of Two Arrays II.java

---
+++ Intersection of Two Arrays II.java
@@ -0,0 +1,52 @@
+/*
+Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2,2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [4,9]
+Explanation: [9,4] is also accepted.
+ 
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+
+*/
+
+import java.util.*;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // mark used elements in nums2
+        boolean[] used = new boolean[nums2.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            for (int j = 0; j < nums2.length; j++) {
+                if (nums1[i] == nums2[j] && !used[j]) {
+                    list.add(nums1[i]);   // store match
+                    used[j] = true;       // mark as used
+                    break;               // move to next nums1 element
+                }
+            }
+        }
+
+        // convert ArrayList → int[]
+        int[] result = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            result[i] = list.get(i);
+        }
+
+        return result;
+    }
+}
